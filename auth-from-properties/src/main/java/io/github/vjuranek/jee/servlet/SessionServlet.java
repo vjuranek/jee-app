4bacdf6663ec44ae04552602140179d2f00e66a2
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-08-25 22:23:56 +0200

    Use annotations for servlet config

--- auth-from-properties/src/main/java/io/github/vjuranek/jee/servlet/SessionServlet.java
+++ auth-from-properties/src/main/java/io/github/vjuranek/jee/servlet/SessionServlet.java
@@ -3,10 +3,18 @@ package io.github.vjuranek.jee.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.HttpMethodConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/session")
+@ServletSecurity(value = @HttpConstraint(rolesAllowed = { "admin" }), httpMethodConstraints = {
+		@HttpMethodConstraint(value = "GET", rolesAllowed = { "admin", "user" }),
+		@HttpMethodConstraint(value = "POST", rolesAllowed = { "admin" })})
 public class SessionServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
