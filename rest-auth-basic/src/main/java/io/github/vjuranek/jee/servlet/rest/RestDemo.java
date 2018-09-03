7173dadc77df56a9116061d1ff0cbe2c07855280
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-09-03 23:32:44 +0200

    Add security contect and annotation based access

--- rest-auth-basic/src/main/java/io/github/vjuranek/jee/servlet/rest/RestDemo.java
+++ rest-auth-basic/src/main/java/io/github/vjuranek/jee/servlet/rest/RestDemo.java
@@ -1,12 +1,25 @@
 package io.github.vjuranek.jee.servlet.rest;
+
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
 
-@Path("/auth")
+@Path("/")
+@RolesAllowed({ "admin" })
 public class RestDemo {
-	
-    @GET
-    public String hello() {
+
+	@Path("auth")
+	@GET
+	public String hello() {
 		return "hello";
+	}
+
+	@Path("ident")
+	@GET
+	@RolesAllowed({ "admin", "user" })
+	public String getIdentity(@Context SecurityContext secCtx) {
+		return "Principal name: " + secCtx.getUserPrincipal().getName();
 	}
 }
